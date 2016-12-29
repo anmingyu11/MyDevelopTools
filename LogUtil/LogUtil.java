d0c11990edac8e14fd97fafe374f8b8cd95577ad
anmingyu <349047303@qq.com>
2016-12-29 18:03:14 +0800

    Update LogUtil
    Signed-off-by: anmingyu <349047303@qq.com>

--- LogUtil/LogUtil.java
+++ LogUtil/LogUtil.java
@@ -1,13 +1,18 @@
-// TODO Copyright
-package org.videolan.vlc.util;
-
-import android.util.Log;
-
-import java.util.Locale;
-
 public class LogUtil {
-    public static boolean ENABLE_DEBUG = true;
+    private static boolean ENABLE_DEBUG = true;
+    public static String TAG = " AMY ";
 
+    public static void setTAG(String tag) {
+        TAG = tag;
+    }
+
+    public static void enableDebug(boolean enable) {
+        ENABLE_DEBUG = enable;
+    }
+
+    public static void enableDownloaderDebug(boolean enable) {
+        DownloaderLog.ENABLE_DEBUG = enable;
+    }
 
     private static String getTag() {
         StackTraceElement[] trace = new Throwable().fillInStackTrace()
@@ -42,36 +47,70 @@ public class LogUtil {
 
     public static void v(String msg) {
         if (ENABLE_DEBUG) {
-            Log.v(getTag(), buildMessage(msg));
+            Log.v(getTag() + TAG, buildMessage(msg));
         }
     }
 
     public static void d(String msg) {
         if (ENABLE_DEBUG) {
-            Log.d(getTag(), buildMessage(msg));
+            Log.d(getTag() + TAG, buildMessage(msg));
         }
     }
 
     public static void i(String msg) {
         if (ENABLE_DEBUG) {
-            Log.i(getTag(), buildMessage(msg));
+            Log.i(getTag() + TAG, buildMessage(msg));
         }
     }
 
     public static void w(String msg) {
         if (ENABLE_DEBUG) {
-            Log.w(getTag(), buildMessage(msg));
+            Log.w(getTag() + TAG, buildMessage(msg));
         }
     }
 
     public static void e(String msg) {
         if (ENABLE_DEBUG) {
-            Log.e(getTag(), buildMessage(msg));
+            Log.e(getTag() + TAG, buildMessage(msg));
         }
     }
 
-    public static void printTraceStack(String msg){
-        Exception e = (msg != null) ? new Exception(msg) : new Exception("Stack state");
-        e.printStackTrace();
+    public static void printTraceStack(String msg) {
+        if (ENABLE_DEBUG) {
+            Exception e = new Exception();
+            StackTraceElement[] steArray = e.getStackTrace();
+            Log.e(TAG, "---------------- Stack Trace ---------------");
+            for (StackTraceElement ste : steArray) {
+                Log.e(TAG, ste.toString());
+            }
+        }
     }
+
+    public static long currentTimeMillis() {
+        long time = System.currentTimeMillis();// ms
+        return time;
+    }
+
+    public static void duration(String msg, long start, long end) {
+        Log.e(TAG, buildMessageThread(msg) + " , duration: " + (end - start) + "ms");
+    }
+
+    private static String buildMessageThread(String msg) {
+        StackTraceElement[] trace = new Throwable().fillInStackTrace()
+                .getStackTrace();
+        String caller = "";
+        for (int i = 2; i < trace.length; i++) {
+            Class<?> clazz = trace[i].getClass();
+            if (!clazz.equals(LogUtil.class)) {
+                caller = trace[i].getMethodName();
+                break;
+            }
+        }
+        Thread thread = Thread.currentThread();
+        return String.format(Locale.US, "%s (%s): %s",
+                thread.toString(),
+                caller,
+                msg);
+    }
+
 }
